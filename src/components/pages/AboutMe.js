21bf771459589b6583cffe1a531dd5b0ac1a859c
Jesse Marino <jessemarino2020@gmail.com>
2022-05-31 16:16:18 -0700

    updated resume, aboutme, and portfolio

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -3,14 +3,17 @@ import React from "react";
 export default function AboutMe() {
   return (
     <div>
-      <h1>About Page</h1>
+      <h1>About Me</h1>
       <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+        Previously a calibration and repair technician for five years. I started
+        my carrer during my time in the Marine Corps while stationed on Camp
+        Pendelton. After that I began working for Avalon Test Equipment where I
+        gained tons of experiance in the telecommunitcation and RF side of
+        Metrology
+        <br />I decieded that I needed a change of pace and since I'm always
+        trying to learn and gain new skill sets, I enrolled in UCSD's Coding
+        Bootcamp. Where I hope to gain all of the skills I will need to become a
+        sucsessful full stack devloper.
       </p>
     </div>
   );
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -3,18 +3,12 @@ import React from "react";
 export default function Resume() {
   return (
     <div>
-      <h1>Home Page</h1>
+      <h1>Resume</h1>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-        velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-        ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-        non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-        ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-        rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-        tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-        porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-        vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-        fames ac ante ipsum primis in faucibus.
+        Download my Resume{" "}
+        <a href="https://docs.google.com/document/d/1AqBVtRPNnAkyK1Fe3gLh437tizIAt2GhSdUeCncXYe8/edit?usp=sharing">
+          here
+        </a>
       </p>
     </div>
   );
