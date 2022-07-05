0e53844235e08ba9b60999f7662c1b43c590dd80
Jesse Marino <jessemarino2020@gmail.com>
2022-07-05 11:21:15 -0700

    updated aboutMe and updated styling of header

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -9,10 +9,11 @@ export default function AboutMe() {
       <div>
         <p className="aboutWords">
           New to the coding world! I have recently graduated from UCSD's Coding
-          Bootcamp as of June 2022! I love to solve problems and create new
-          things which is why I decided to get into Web Development. Previously
-          I was in the Metrology field working as a Calibration and Repair
-          Technician for over five years. <br />
+          Bootcamp as of June 2022! I love problem solving, thinking out side of
+          the box, and making new ideas come to life which is why I decided to
+          get into Web Development. Previously I was in the Metrology field
+          working as a Calibration and Repair Technician for over five years.{" "}
+          <br />
           While working for my last employer I was exposed to Python. We used
           Python to create automated testing procedures for Rohde and Schwarz,
           Keysight, Tektronix, LeCroy, Kiethley, and National Instruments
