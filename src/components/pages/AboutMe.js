9191ee38dd3f436d443adc4c50d853d679a0e73a
Jesse Marino <jessemarino2020@gmail.com>
2022-07-08 15:35:40 -0700

    updated about me

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -8,18 +8,26 @@ export default function AboutMe() {
       </div>
       <div>
         <p className="aboutWords">
-          New to the coding world! I have recently graduated from UCSD's Coding
-          Bootcamp as of June 2022! I love problem solving, thinking out side of
-          the box, and making new ideas come to life which is why I decided to
-          get into Web Development. Previously I was in the Metrology field
-          working as a Calibration and Repair Technician for over five years.{" "}
+          New to the coding world and recent graduate of UCSD's Coding Bootcamp!
+          I love problem solving, thinking out side of the box, making new ideas
+          come to life, and challenging myself which is why I decided to get
+          into Web Development. Previously a Metrologist but have now set out to
+          begin a new career as a Full Stack Web Developer.
           <br />
-          While working for my last employer I was exposed to Python. We used
-          Python to create automated testing procedures for Rohde and Schwarz,
-          Keysight, Tektronix, LeCroy, Kiethley, and National Instruments
-          precision test equipment. This is were my interests in coding began!
-          Since then I have learned so many new skills and I am beyond excited
-          to learn even more and grow as a Full Stack Web Developer.
+          <br />
+          My intrests in coding first started when I was exposed to Python by a
+          previous employer. We were using Python to create automated test
+          scripts for Keysight, Tektronix, LeCroy, Kiethley, and National
+          Instruments precision test equipment. I was amazed by the ability to
+          drastically improve productivity and saw the endless possiblities this
+          tool could provide.
+          <br />
+          <br />
+          While working with these scripts my curiousity grew, so I did some
+          research to see what else was out there. I was quick to find that I
+          hadn't even begain to scratch the surface of "coding" and is why I
+          enrolled in UCSD's Coding Bootcamp. I am beyond exited to grow in this
+          field and be apart of an awesome community!
         </p>
       </div>
     </>
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -21,8 +21,7 @@ export default function Portfolio() {
             <h5 className="card-title">Code Quiz</h5>
             <p className="card-text">
               This app is for users that want to test their knowledge on some
-              basic coding questions. This is a timed quiz and if you answer
-              incorrectly you will have time deducted.
+              basic coding questions.
             </p>
             <a
               href="https://jessemarino.github.io/do-you-even-code/"
@@ -38,8 +37,8 @@ export default function Portfolio() {
           <div className="card-body">
             <h5 className="card-title">Crypto PunX</h5>
             <p className="card-text">
-              Check out Crypto Punx! This app is for users that are new
-              investing in crypto and are trying to get into the crypto world.
+              Check out Crypto Punx! This app is for users that are new to
+              investing and are trying to get into the crypto world.
             </p>
             <a
               href="https://guarded-scrubland-93446.herokuapp.com"
@@ -54,12 +53,7 @@ export default function Portfolio() {
           <img src={note} className="card-img-top" alt="calculator" />
           <div className="card-body">
             <h5 className="card-title">Take a Note</h5>
-            <p className="card-text">
-              This app is for users that need to take a few notes through-out
-              the day. Write a note down and it will be saved. If you need to
-              view it all you have to do is click on the note. Once your
-              finished with the note you can delete it.
-            </p>
+            <p className="card-text">Try out this note taking app!</p>
             <a
               href="https://limitless-sea-73688.herokuapp.com/"
               className="btn btn-dark"
@@ -73,30 +67,9 @@ export default function Portfolio() {
           <img src={textPic} className="card-img-top" alt="text-editor" />
           <div className="card-body">
             <h5 className="card-title">Text Editor</h5>
-            <p className="card-text">
-              Check out this text editor! Once opened you can add text that will
-              stay on app even if you leave or refresh.
-            </p>
+            <p className="card-text">Check out this text editor!</p>
             <a
               href="https://infinite-headland-27852.herokuapp.com/"
-              className="btn btn-dark"
-            >
-              Check it out!
-            </a>
-          </div>
-        </div>
-
-        <div className="card" style={{ width: "18rem" }}>
-          <img src={golfPic} className="card-img-top" alt="golfopida" />
-          <div className="card-body">
-            <h5 className="card-title">Golfapedia</h5>
-            <p className="card-text">
-              Golfapedia is an app for users that want to get into golfing.
-              Enter a city and it will show all golf courses in your area along
-              with the weather.{" "}
-            </p>
-            <a
-              href="https://nirav-v.github.io/golfapedia/"
               className="btn btn-dark"
             >
               Check it out!
@@ -113,8 +86,8 @@ export default function Portfolio() {
           <div className="card-body">
             <h5 className="card-title">Password Generator</h5>
             <p className="card-text">
-              This is app is for users that want to make secure passwords. Users
-              will be prompted a few questions then be given a password.
+              This is for users that want to make secure passwords. Users will
+              be prompted a few questions then be given a password.
             </p>
             <a
               href="https://jessemarino.github.io/the_super_generator/"
@@ -133,10 +106,9 @@ export default function Portfolio() {
           <div className="card-body">
             <h5 className="card-title">Daily Scheduler</h5>
             <p className="card-text">
-              This app is for users that need to keep track of their daily
-              tasks. This planner has color codes for the hour. Grey means the
-              hour has past, red means that is the current hour, and green
-              meaning that hour is to come.
+              This is a Daily Scheduling app that has color coordinators for the
+              hour. Grey means the hour has past, red means that is the current
+              hour, and green meaning that hour is to come.
             </p>
             <a
               href="https://jessemarino.github.io/key_to_a_great_day/"
